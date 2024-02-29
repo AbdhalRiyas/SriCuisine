dec13337427042d493f18acce850cc87ddb27e84
Thivina2002 <136422598+thivinakudagama@users.noreply.github.com>
2024-02-29 21:43:37 +0530

    Added the dropdown option for the fruitsingredientScreen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_application_1/Pages.dart/login_page.dart';
 import 'package:flutter_application_1/Pages.dart/intro_page.dart';
 import 'package:flutter_application_1/Pages.dart/specification_page.dart';
 
+import 'package:flutter_application_1/Pages.dart/available_ingredients_screen.dart';
 import 'package:flutter_application_1/Pages.dart/FruitIngredientsScreen.dart';
 import 'package:flutter_application_1/Pages.dart/DairyProductsIngredientsScreen.dart';
 import 'package:flutter_application_1/Pages.dart/MeatIngredientsScreen.dart';
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:
-          //Recipes(),
+      home: //Recipes(),
+
           //SpecPage(),
           //IntroPage(),
           //LoginPage(),
@@ -33,8 +34,8 @@ class MyApp extends StatelessWidget {
           //AvailableIngredientsScreen(),
           //VegetableIngredientsScreen(),
           //MeatIngredientsScreen(),
-          //FruitIngredientsScreen(),
-          DairyProductsIngredientsScreen(),
+          FruitIngredientsScreen(),
+      //DairyProductsIngredientsScreen(),
     );
   }
 }
