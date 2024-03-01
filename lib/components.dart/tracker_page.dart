510bd8afcf0bc2acc013beac04a50806221274ac
NidhyaKA <158128832+NidhyaKA@users.noreply.github.com>
2024-03-01 17:36:34 +0530

    added tracker page

---
+++ lib/components.dart/tracker_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: IngredientsPage()));
+}
+
+// ignore: use_key_in_widget_constructors
+class IngredientsPage extends StatefulWidget {
+  List<Ingredient> ingredients = <Ingredient>[];
+
+  setIngredients(List<Ingredient> newIngredients) {
+    setState(() {
+      ingredients = newIngredients;
+    });
+  }
+
+  @override
+  _IngredientsPageState createState() => _IngredientsPageState();
+  
+  void setState(Null Function() param0) {}
+}
+
+class _IngredientsPageState extends State<IngredientsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ingredients'),
+      ),
+      body: ListView.builder(
+        itemCount: widget.ingredients.length,
+        itemBuilder: (context, index) {
+          final ingredient = widget.ingredients[index];
+          return Card(
+            child: ListTile(
+              title: Text(ingredient.name),
+              subtitle: Text('Expires on: ${ingredient.expirationDate}'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Ingredient {
+  String name;
+  DateTime expirationDate;
+
+  Ingredient({required this.name, required this.expirationDate});
+}
