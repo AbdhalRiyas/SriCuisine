9206ea3f1b2fe563d6097ac74fd0e18814852482
Nadil Malalanayake <nadilviksura@gmail.com>
2024-03-16 18:18:13 +0530

    added welcome page

--- frontend/lib/pages.dart/welcomepage.dart
+++ frontend/lib/pages.dart/welcomepage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages.dart/login.dart';
 import 'package:flutter_application_1/pages.dart/signup.dart';
 
-class HomePage extends StatelessWidget {
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +16,6 @@ class HomePage extends StatelessWidget {
           height: MediaQuery.of(context).size.height,
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
           child: Column(
-
             // even space distribution
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -28,33 +27,27 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30,
-
                     ),
-                    
                   ),
                   const SizedBox(
                     height: 20,
                   ),
-
-                Text("hello welcome to SriCusine recipe recommendation application",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 15,
-                  ),
+                  Text(
+                    "hello welcome to SriCusine recipe recommendation application",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 15,
+                    ),
                   )
                 ],
               ),
-
               Container(
                 height: MediaQuery.of(context).size.height / 3,
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("images/appLogo.jpg")
-                  )
-                ),
+                    image: DecorationImage(
+                        image: AssetImage("images/appLogo.jpg"))),
               ),
-
               Column(
                 children: <Widget>[
                   // the login button
@@ -62,55 +55,46 @@ class HomePage extends StatelessWidget {
                     minWidth: double.infinity,
                     height: 60,
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
                     },
 
                     // defining the shape
                     shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.black
-                      ),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
+                        side: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
 
                     child: Text(
                       "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
                     ),
                   ),
 
                   // creating the signup button
-                  SizedBox(height:20),
+                  SizedBox(height: 20),
                   MaterialButton(
                     minWidth: double.infinity,
                     height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignupPage()));
                     },
-                    
                     color: Color(0xff0095FF),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)
-                    ),
+                        borderRadius: BorderRadius.circular(50)),
                     child: Text(
                       "Sign up",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18),
                     ),
                   )
-
                 ],
               )
-
-
-
             ],
           ),
         ),
@@ -118,4 +102,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
