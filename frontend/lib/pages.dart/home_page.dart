bf04fa46350a3950904da11931a6190d48e0d40a
Thivina2002 <136422598+thivinakudagama@users.noreply.github.com>
2024-03-17 23:40:47 +0530

    created the home page

--- frontend/lib/pages.dart/home_page.dart
+++ frontend/lib/pages.dart/home_page.dart
@@ -1,31 +1,88 @@
 import 'package:flutter/material.dart';
 
-class  HomePage extends StatefulWidget {
-  @override
-  _Home createState() => _Home();
-}
-
-class _Home extends State< HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    double screenWidth = MediaQuery.of(context).size.width;
+    double containerSize =
+        (screenWidth - 40) / 2; // Subtracting padding and spacing
+
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.home),
-              SizedBox(width: 10),
-              Text('Home')
-            ]),
+            children: [Icon(Icons.home), SizedBox(width: 10), Text('Home')]),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to SriCuisine',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: containerSize,
+                  height: containerSize,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.yellow,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'BMI',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  width: containerSize,
+                  height: containerSize,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.yellow,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Calorie',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: 390,
+              height: 100,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.yellow, // Making color transparent
+              ),
+              child: const Center(
+                child: Text(
+                  'Rectangle',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
