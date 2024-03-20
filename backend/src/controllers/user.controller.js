3294d10db614c2e1f37e602e7ebd8393b695b00d
Sanumi Hanwella <134194889+Sanumi2002@users.noreply.github.com>
2024-03-21 01:53:16 +0530

    added ingredients route and edit,delete methods in the user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -109,3 +109,134 @@ const requestPasswordReset = asyncHanlder(async (req, res) => {
 
     logger.trace("[authController] :: requestPasswordReset() : End");
 })
+// @desc    edit a user
+// @route   PUT /api/v1/users/:id
+// @access  Private
+const editUser = asyncHandler(async (req, res) => {
+    logger.trace("[projectController] :: editUser() : Start");
+
+    const userId =req.body.userId
+    
+    if (!userId){
+        logger.error("[userController] :: editUser() : user id is a must");
+        throw new AppError(400, i18n.__("UNAUTHORIZED"));
+    }
+
+    let updates = {
+        ...(req.body.userName !== null && req.body.userName != "" && { userName: req.body.userName }),
+        ...(req.body.email !== null && req.body.email != "" && { email: req.body.email }),
+        ...(req.body.allergens && Array.isArray(req.body.allergens) && { allergens: req.body.allergens }) 
+    }
+
+    updates.age = req.body.age;
+    updates.weight = req.body.weight;
+    updates.height = req.body.height;
+
+    const userNameRegex = /^[^\s]{4,100}$/;
+        if (!userNameRegex.test(updates.userName)) {
+            logger.error("[userController] :: editUser() : Invalid username format");
+            throw new AppError(400, i18n.__("ERROR_INVALID_USERNAME_FORMAT"));
+        }
+
+
+    if (updates.userName !== undefined) {
+        const userNameExists = await User.find({
+            userName: updates.userName,
+            _id: { $ne: currentUser }
+        });
+
+        if (userNameExists.length > 0) {
+            logger.error("[userController] :: editUser() : Username already exists");
+            throw new AppError(400, i18n.__("ERROR_USER_ALREADY_EXISTS"));
+        }
+    }
+
+
+    const emailRegex = /^([a-zA-Z0-9_\.-]+)@([a-zA-Z0-9_\.-]+)\.([a-zA-Z]{2,6})$/;
+    if (!emailRegex.test(updates.email)) {
+        logger.error("[userController] :: editUser() : Invalid email format");
+        throw new AppError(400, i18n.__("ERROR_INVALID_EMAIL_FORMAT"));
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true })
+
+    if (!updatedUser) {
+        logger.error("[userController] :: editUser() : No users with the given id");
+        throw new AppError(404, i18n.__("USER_NOT_FOUND"))
+    }
+
+    res.status(200).json(updatedUser);
+    logger.trace("[userController] :: editUser() : End");
+})
+
+
+// @desc Delete user
+// @route DELETE /api/v1/users/:id
+// @access Private
+const deleteUser = asyncHandler(async (req, res) => {
+    logger.trace("[userController] :: deleteUser() : Start");
+
+    const userId = req.params.id;
+
+    const result = await User.deleteOne({ _id: userId })
+
+    if (!result.deletedCount) {
+        logger.error("[userController] :: deleteUser() : No users with the given id");
+        throw new AppError(404, i18n.__("USER_NOT_FOUND"))
+    }
+    res.status(200).json({ payload: null, status: Status.getSuccessStatus(i18n.__("DELETE_SUCCESS")) });
+    logger.trace("[userController] :: deleteUser() : End");
+})
+
+
+// const resetPassword = asyncHandler(async (req, res) => {
+//     const { resetPasswordToken, newPassword } = req.body;
+  
+//     const user = await User.findOne({
+//       resetPasswordToken,
+//       resetPasswordExpires: { $gt: Date.now() },
+//     });
+  
+//     if (!user) {
+//       throw new AppError('Invalid or expired password reset token', 400);
+//     }
+  
+//     user.password = await bcrypt.hash(newPassword, 10);
+//     user.resetPasswordToken = undefined;
+//     user.resetPasswordExpires = undefined;
+  
+//     await user.save();
+  
+//     res.status(200).json({ message: 'Password reset successful' });
+//   });
+
+  
+// const requestPasswordReset = asyncHandler(async (req, res) => {
+//     const { userName } = req.body; 
+//     const user = await User.findOne({ userName }); 
+//     if (!user) {
+//       throw new AppError('User not found', 404);
+//     }
+  
+//     const resetToken = basicUtil.generateResetPasswordToken();
+//     user.resetPasswordToken = resetToken;
+//     user.resetPasswordExpires = Date.now() + 3600000; 
+  
+//     await user.save();
+
+  
+//     await basicUtil.sendPasswordResetEmail(user.email, resetToken); 
+//     res.status(200).json({ message: 'Password reset email sent' });
+    
+// });
+
+module.exports = {
+    createUser,
+    editUser,
+    deleteUser,
+    getUsers,
+    getUserById
+    // loginUser,
+    // resetPassword,
+    // requestPasswordReset
+}
