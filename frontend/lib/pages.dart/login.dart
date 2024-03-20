11bdc834d779873e85fdf61d4ab5decda94d9c3e
Nadil Malalanayake <nadilviksura@gmail.com>
2024-03-20 14:26:26 +0530

    Validated login and signup text fields

--- frontend/lib/pages.dart/login.dart
+++ frontend/lib/pages.dart/login.dart
@@ -1,219 +1,205 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages.dart/otpverify.dart';
 import 'package:flutter_application_1/pages.dart/main_screen.dart';
-import 'package:flutter_application_1/pages.dart/otpverify.dart';
 import 'package:flutter_application_1/pages.dart/signup.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formfield = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        //brightness: Brightness.light,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black,
+        appBar: AppBar(
+          title: Text("Login Page"),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
           ),
         ),
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: Form(
+                key: _formfield,
                 child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    const Text(
-                      "Login",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Login to your account",
-                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                    )
-                  ],
-                ),
-
-                //text field topic display
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
-                    children: <Widget>[
-                      inputFile(label: "Username"),
-                      inputFile(label: "Password", obscureText: true)
-                    ],
-                  ),
-                ),
-
-                // // forgot password text padding
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        child: const Text(
-                          'forgot password',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (BuildContext) => OtpPage()));
-                        },
-                      )
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 15),
-
-                //login button padding and edit
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Container(
-                    padding: EdgeInsets.only(top: 2, left: 2),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: const Border(
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-                        )),
-
-                    //Login button Navigation
-
-                    child: MaterialButton(
-                      minWidth: double.infinity,
-                      height: 60,
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(context,
-                              MaterialPageRoute(builder: (context) => MainScreen()),
-                                (Route<dynamic> route) => false,
-                        );
-                      },
-                      color: Color(0xff0095FF),
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-
-                      //Login button text
-
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 13),
-
-                //dont have an account text
-
-                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Don't have an account?"),
-                    GestureDetector(
-                      child: const Text(
-                        "  Sign Up",
-                        style: TextStyle(
-                            color: Colors.blue, fontWeight: FontWeight.bold),
+                  children: [
+                    Image.asset("images/whiteLogo.jpg",
+                    height: 150,
+                    width: 200,
+                    ),
+                    const SizedBox(height: 10),
+
+                    // Username Validate part
+
+                    TextFormField(
+                        keyboardType: TextInputType.name,
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          labelText: "UserName",
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.person)
+                        ),
+                        validator: (value) {
+                          bool userNameValid =
+                              RegExp(r'^[a-z A-Z]+$').hasMatch(value!);
+
+                          if (value.isEmpty) {
+                            return "Enter userName";
+
+                          } else if (!userNameValid) {
+                            return "Enter Valid UserName";
+                          }
+                        }),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    // Password Validate part
+
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: passController,
+                      obscureText: passToggle,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                        suffix: InkWell(
+                          onTap: () {
+                            setState(() {
+                              passToggle = !passToggle;
+                            });
+                          },
+                          child: Icon(passToggle
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
                       ),
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (BuildContext) => SignupPage()));
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Password";
+                        } else if (passController.text.length < 6) {
+                          return "Password length should not be more than 6 Characters";
+                        }
                       },
-                    )
+                    ),
+
+                    const SizedBox(height: 30),
+
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 2.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            child: const Text(
+                              'forgot password',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                            onTap: () {
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (BuildContext) => OtpPage()));
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    InkWell(
+                      onTap: () {
+                        if (_formfield.currentState!.validate()) {
+                          print("Data Addeed Successfully");
+                          usernameController.clear();
+                          passController.clear();
+
+                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext)=>MainScreen()));
+                        }
+                         
+                      },
+
+                      //button editing
+
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)),
+                        child:const  Center(
+                            child: Text(
+                          "Log in",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25,),
+
+                    //dont have an account text
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        Text("Don't have an account?"),
+                        GestureDetector(
+                          child: const Text(
+                            "  Sign Up",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (BuildContext) => SignupPage()));
+                          },
+                        )
+                      ],
+                    ),
+
+                    const SizedBox(height: 5),
                   ],
-                ),
-
-                const SizedBox(height: 5),
-
-                // login page down image
-
-                Container(
-                  padding: EdgeInsets.only(top: 100),
-                  height: 180,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("images/foodPan.jpg"),
-                          fit: BoxFit.fitHeight)),
                 )
-              ],
-            ))
-          ],
-        ),
-      ),
-    );
+              ),
+          ),
+        )
+        
+      );
   }
 }
+       
 
-// creating a widget for text field
 
-Widget inputFile({label, obscureText = false}) {
-  //label text style edit
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: const TextStyle(
-            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
-      ),
 
-      const SizedBox(height: 5), //text and text field between space
-
-      //text fied edited stuff
-      TextField(
-        obscureText: obscureText,
-        decoration: const InputDecoration(
-            contentPadding: EdgeInsets.symmetric(
-                vertical: 0, horizontal: 10), //text fild size
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color:
-                      Color.fromARGB(255, 175, 165, 165) // display border color
-                  ),
-            ),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: Color.fromARGB(
-                        255, 172, 151, 151)) //when user clicked popup color
-                )),
-      ),
-
-      const SizedBox(
-        height: 10,
-      ) //text field between space
-    ],
-  );
-}
+                   
--- frontend/lib/pages.dart/signup.dart
+++ frontend/lib/pages.dart/signup.dart
@@ -1,188 +1,200 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages.dart/login.dart';
-import 'package:flutter_application_1/pages.dart/specification_page.dart';
+import 'package:flutter_application_1/Pages.dart/otpverify.dart';
+import 'package:flutter_application_1/pages.dart/signup.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
+  @override
+  State<SignupPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<SignupPage> {
+  final _formfield = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        //brightness: Brightness.light,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black,),
-        ),
-      ),
-
-
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          height: MediaQuery.of(context).size.height - 50,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  const Text("Sign up",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-
-                  ),
-                  ),
-
-                  const SizedBox(height: 20,),
-                  Text("Create an account, It's free ",
-                    style: TextStyle(
-                        fontSize: 15,
-                        color:Colors.grey[700]),)
-
-
-                ],
-              ),
-
-
-            //text field topic display
-
-              Column(
-                children: <Widget>[
-                  inputFile(label: "Username"),
-                  inputFile(label: "Email"),
-                  inputFile(label: "Password", obscureText: true),
-                  inputFile(label: "Confirm Password ", obscureText: true),
-                ],
-              ),
-
-
-              //signup button padding and edit
-
-              Container(
-                padding: EdgeInsets.only(top: 2, left: 2),
-                decoration:
-                BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    border: const Border(
-                      bottom: BorderSide(color: Colors.black),
-                      top: BorderSide(color: Colors.black),
-                      left: BorderSide(color: Colors.black),
-                      right: BorderSide(color: Colors.black),
-                    )
-                ),
-
-                //signup button Navigation
-
-                child: MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SpecPage()));
-                  },
-                  color: Color(0xff0095FF),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-
-                //signup button text
-
-                  child: const Text(
-                    "Sign up", style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Colors.white,
-
-                  ),
-                  ),
-
-                ),
-              ),
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text("Already have an account?"),
-                  GestureDetector(
-                  child:const Text(
-                  " Log in",
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.bold),
-
-                ),
-                onTap : (){
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext)=>LoginPage()));
-                  },
-                )
-                ],
-              )
-
-
-            ],
-
-          ),
-
-
-        ),
-
-      ),
-
-    );
-  }
-}
-
-
-
-//creating a widget for text field
-
-Widget inputFile({label, obscureText = false})
-{
-
-  //label text style edit
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style:const TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.w400,
-          color:Colors.black87
-        ),
-
-      ),
-
-      const SizedBox( height: 5),  //text and text field between space
-
-      //text fied edited stuff
-      TextField(
-        obscureText: obscureText,
-        decoration: const InputDecoration(
-          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10), //text fild size
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color.fromARGB(255, 175, 165, 165)  // display border color
+        appBar: AppBar(
+          title: Text("SignUp  Form"),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
             ),
           ),
-
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color:  Color.fromARGB(255, 172, 151, 151))  //when user clicked popup color
-          )
         ),
-      ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: Form(
+                key: _formfield,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Image.asset("images/whiteLogo.jpg",
+                    // height: 150,
+                    // width: 200,
+                    // ),
+                    const SizedBox(height: 10),
 
-      const SizedBox(height: 10,)  //text field between space
+                    // Username Validate part
 
-    ],
-  );
+                    TextFormField(
+                        keyboardType: TextInputType.name,
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          labelText: "UserName",
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.person)
+                        ),
+                        validator: (value) {
+                          bool userNameValid =
+                              RegExp(r'^[a-z A-Z]+$').hasMatch(value!);
+
+                          if (value.isEmpty) {
+                            return "Enter userName";
+                          } else if (!userNameValid) {
+                            return "Enter Valid UserName";
+                          }
+                        }),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    //email validation
+
+                    TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: "Email Address",
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                        validator: (value) {
+                          bool emailValid = RegExp(
+                                  r'^([a-zA-Z0-9_\.-]+)@([a-zA-Z0-9_\.-]+)\.([a-zA-Z]{2,6})$')
+                              .hasMatch(value!);
+
+                          if (value.isEmpty) {
+                            return "Enter emailaddress";
+                          } else if (!emailValid) {
+                            return "Enter Valid emailAddress";
+                          }
+                        }),
+
+                        const SizedBox(
+                      height: 20,
+                    ),
+
+
+                    // Password Validate part
+
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: passController,
+                      obscureText: passToggle,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                        suffix: InkWell(
+                          onTap: () {
+                            setState(() {
+                              passToggle = !passToggle;
+                            });
+                          },
+                          child: Icon(passToggle
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Password";
+                        } else if (passController.text.length < 6) {
+                          return "Password length should not be more than 6 Characters";
+                        }
+                      },
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    InkWell(
+                      onTap: () {
+                        if (_formfield.currentState!.validate()) {
+                          print("Data Addeed Successfully");
+                          usernameController.clear();
+                          emailController.clear();
+                          passController.clear();
+                        }
+                      },
+
+                      //button editing
+
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Center(
+                            child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 25,
+                    ),
+
+                    //dont have an account text
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an Account?"),
+                        GestureDetector(
+                          child: const Text(
+                            " Log in",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (BuildContext) => Login()));
+                          },
+                        )
+                      ],
+                    ),
+
+                    const SizedBox(height: 5),
+                  ],
+                )),
+          ),
+        ));
+  }
 }
