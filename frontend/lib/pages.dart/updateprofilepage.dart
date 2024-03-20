af0625aa38b60599e7af52f79ca3b79540e7a76e
NidhyaKA <158128832+NidhyaKA@users.noreply.github.com>
2024-03-20 13:36:38 +0530

    updates for the text fields in the profile page

--- frontend/lib/pages.dart/updateprofilepage.dart
+++ frontend/lib/pages.dart/updateprofilepage.dart
@@ -7,6 +7,132 @@ class UpdateProfilePage
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.yellow,
+        title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.edit),
+              SizedBox(width: 10),
+              Text('Edit Profile')
+            ]),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 130,
+                height: 130,
+                child: ClipRect(
+                  child: Image.asset('assets/images/profile.jpg'),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text("User Name",
+                  style: TextStyle(
+                      fontFamily: 'inter',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black)),
+              Text("Email@email.com",
+                  style: TextStyle(
+                      fontFamily: 'inter',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black)),
+              const SizedBox(height: 20),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.orange[50],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Repeat the above code for other TextFormField widgets
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.orange[50],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Height (cm)',
+                      labelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.orange[50],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Weight (kg)',
+                      labelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 2),
+                      ),
+                      filled: true,
+                      fillColor: Colors.orange[50],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      )
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(130, 65),
+                      backgroundColor: Colors.yellow,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
