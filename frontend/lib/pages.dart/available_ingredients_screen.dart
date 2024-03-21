dc7f1672088ab5e73f20ef72e505b06a3404675a
Thivina2002 <136422598+thivinakudagama@users.noreply.github.com>
2024-03-22 03:08:23 +0530

    made the changes for the ingredients page by storing the selected ingredients in an array

--- frontend/lib/pages.dart/available_ingredients_screen.dart
+++ frontend/lib/pages.dart/available_ingredients_screen.dart
@@ -1,9 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages.dart/VegetableIngredientsScreen.dart';
-import 'package:flutter_application_1/pages.dart/FruitIngredientsScreen.dart';
-import 'package:flutter_application_1/pages.dart/MeatIngredientsScreen.dart';
-import 'package:flutter_application_1/pages.dart/StaplesIngredientsScreen.dart';
 
 class AvailableIngredientsScreen extends StatefulWidget {
   @override
@@ -22,33 +17,135 @@ class _AvailableIngredientsScreenState
     'Staples',
   ];
 
+  final List<List<String>> _ingredients = [
+    // Vegetables
+    [
+      'Mushrooms',
+      'Raddish',
+      'Cauliflower',
+      'Broccoli',
+      'Egg-plant',
+      'Spinach',
+      'Cucumber',
+      'Bell Pepper',
+      'Bitter-Gourd',
+      'Tomatoes',
+      'Carrots',
+      'Green Beans',
+      'Pumpkin',
+      'Cabbage',
+      'Potatoes',
+      'Coriander',
+    ],
+    // Meats
+    [
+      'Chicken',
+      'Pork',
+      'Mutton',
+      'Fish',
+      'Beef',
+      'Lamb',
+      'Turkey',
+      'Prawns',
+      'Crab',
+      'Clamps',
+    ],
+    // Fruits
+    [
+      'Apple',
+      'Avocado',
+      'Banana',
+      'Blueberry',
+      'Cactus fruit',
+      'Cantaloupe',
+      'Cherry',
+      'Clementine',
+      'Dates',
+      'Eggplant',
+      'Ginger Root',
+      'Grape',
+      'Grapefruit',
+      'Guava',
+      'Jackfruit',
+      'Kiwi',
+      'Lemon',
+      'Mandarine',
+      'Mango',
+      'Mangostan',
+      'Mulberry',
+      'Nectarine',
+      'Orange',
+      'Papaya',
+      'Passion fruit',
+      'Peach',
+      'Pears',
+      'Pineapple',
+      'Pomegranate',
+      'Rambutan',
+      'Raspberry',
+      'Redcurrant',
+      'Strawberry',
+      'Watermelon',
+    ],
+    // Staples
+    [
+      'Gram Flour',
+      'Wheat Flour',
+      'Rice Flour',
+      'Rice',
+      'Noodle',
+      'Dhal',
+      'Ghee',
+      'Milk',
+      'Nuts',
+      'Eggs',
+      'Chickpeas',
+      'Greenpeas',
+    ],
+  ];
+
+  List<Map<String, dynamic>> _selectedIngredients = [];
+  bool _showIngredients = false;
+
   void _onCategoryTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      if (_selectedIndex == index) {
+        _showIngredients = !_showIngredients;
+      } else {
+        _selectedIndex = index;
+        _showIngredients = true;
+      }
     });
+  }
 
-    switch (_categories[index]) {
-      case 'Vegetables':
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => VegetableIngredientsScreen()));
-        break;
-      case 'Meats':
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => MeatIngredientsScreen()));
-        break;
-      case 'Fruits':
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => FruitIngredientsScreen()));
-        break;
-      case 'Staples':
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => StaplesIngredientsScreen()));
-        break;
+  void _onIngredientSelected(String ingredient) async {
+    final DateTime? selectedDate = await _selectDate(context);
+    if (selectedDate != null) {
+      setState(() {
+        final existingIndex = _selectedIngredients
+            .indexWhere((item) => item['name'] == ingredient);
+        if (existingIndex != -1) {
+          _selectedIngredients.removeAt(
+              existingIndex); // Deselect the ingredient if it's already selected
+        } else {
+          final Map<String, dynamic> ingredientMap = {
+            'name': ingredient,
+            'date': selectedDate,
+          };
+          _selectedIngredients.add(
+              ingredientMap); // Select the ingredient if it's not already selected
+        }
+      });
     }
+  }
+
+  Future<DateTime?> _selectDate(BuildContext context) async {
+    return await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2030),
+    );
   }
 
   @override
@@ -56,85 +153,175 @@ class _AvailableIngredientsScreenState
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
-        title:
-            const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Icon(CupertinoIcons.square_grid_2x2_fill),
-          SizedBox(width: 10),
-          Text('Available Ingredients')
-        ]),
+        title: const Text(
+          'Available Ingredients',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0), // Add padding
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(_categories.length, (index) {
-              final category = _categories[index];
-              final isSelected = _selectedIndex == index;
-              return Column(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _onCategoryTapped(index);
-                    },
-                    child: Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 12),
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      decoration: BoxDecoration(
-                        color: isSelected
-                            ? const Color(
-                                0xFFE0E0E0) // Light gray for selection
-                            : Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 1,
-                            blurRadius: 3,
-                            offset: const Offset(0, 1),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _categories.length,
+              itemBuilder: (context, index) {
+                final category = _categories[index];
+                final isSelected = _selectedIndex == index;
+                return Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _onCategoryTapped(index);
+                      },
+                      child: Card(
+                        elevation: isSelected ? 4 : 0,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 12),
+                          decoration: BoxDecoration(
+                            color: isSelected
+                                ? const Color(0xFFE0E0E0)
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: isSelected
+                                ? [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 1,
+                                      blurRadius: 3,
+                                      offset: const Offset(0, 1),
+                                    ),
+                                  ]
+                                : null,
                           ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            category,
-                            style: TextStyle(
-                              color: isSelected ? Colors.blue : Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Roboto', // Example custom font
-                            ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                category,
+                                style: TextStyle(
+                                  color:
+                                      isSelected ? Colors.blue : Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                isSelected
+                                    ? _showIngredients
+                                        ? Icons.arrow_drop_up
+                                        : Icons.arrow_drop_down
+                                    : null,
+                                color: isSelected ? Colors.blue : Colors.black,
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }),
-          ),
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SizedBox(
-          width: double.infinity,
-          height: 50,
-          child: ElevatedButton(
-            onPressed: () {
-              //  submit function
-            },
-            child: const Text(
-              'Submit',
-              style: TextStyle(fontSize: 18),
+                    Visibility(
+                      visible: isSelected && _showIngredients,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        height: _showIngredients
+                            ? MediaQuery.of(context).size.height * 0.3
+                            : 0,
+                        child: Center(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: _ingredients[index]
+                                  .map(
+                                    (ingredient) => Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: CheckboxListTile(
+                                        title: Row(
+                                          children: [
+                                            Expanded(
+                                              child: Text(
+                                                '$ingredient ${_selectedIngredients.where((item) => item['name'] == ingredient).isNotEmpty ? '- ${_selectedIngredients.firstWhere((item) => item['name'] == ingredient)['date'].toString().substring(0, 10)}' : ''}',
+                                                style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: _selectedIngredients
+                                                          .any((element) =>
+                                                              element['name'] ==
+                                                              ingredient)
+                                                      ? Colors.blue
+                                                      : Colors.black,
+                                                ),
+                                              ),
+                                            ),
+                                            IconButton(
+                                              onPressed: () {
+                                                print(_selectedIngredients
+                                                    .toString()
+                                                    .substring(0));
+                                                _onIngredientSelected(
+                                                    ingredient);
+                                              },
+                                              icon: Icon(
+                                                Icons.calendar_today,
+                                                color: _selectedIngredients.any(
+                                                        (element) =>
+                                                            element['name'] ==
+                                                            ingredient)
+                                                    ? Colors.blue
+                                                    : Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        value: _selectedIngredients.any(
+                                            (element) =>
+                                                element['name'] == ingredient),
+                                        onChanged: (value) {
+                                          _onIngredientSelected(ingredient);
+                                        },
+                                        controlAffinity:
+                                            ListTileControlAffinity.leading,
+                                        activeColor: Colors.blue,
+                                      ),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0),
+                  ),
+                ),
+                onPressed: () {
+                  //  submit function
+                },
+                child: const Text(
+                  'Generate Recipes',
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
