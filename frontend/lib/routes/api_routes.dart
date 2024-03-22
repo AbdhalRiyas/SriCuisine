a1bc8bad91b13bbe7c28d4f3bc9e59cc4d5c7158
Abdhal <riyas.20220165@iit.ac.lk>
2024-03-22 13:42:56 +0530

    created userapi

---
+++ frontend/lib/routes/api_routes.dart
@@ -0,0 +1,9 @@
+class ApiRoutes {
+  static const String BASE_URL = "http://192.168.1.10:3000";
+  
+  static const String CREATE_USER = "/api/v1/users/createUser";
+  static const String DELETE_USER = "/deleteUser";
+  static const String EDIT_USER = "/editUser";
+  static const String GET_USERS = "/getUsers";
+  static const String GET_USER_BY_ID = "/getUserById";
+}
