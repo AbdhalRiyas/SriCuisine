2dcb7f4b99079878cdc8d46bf230f53b9350d4b8
Abdhal <riyas.20220165@iit.ac.lk>
2024-03-25 06:29:37 +0530

    final commit

--- frontend/lib/pages/login.dart
+++ frontend/lib/pages/login.dart
@@ -23,16 +23,6 @@ class _LoginPageState extends State<LoginPage> {
           centerTitle: true,
           elevation: 0,
           backgroundColor: Colors.white,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back_ios,
-              size: 20,
-              color: Colors.black,
-            ),
-          ),
         ),
         body: SingleChildScrollView(
           child: Padding(
